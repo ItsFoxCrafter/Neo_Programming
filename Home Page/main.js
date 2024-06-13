4f86193aa30b1c39361b61a650dd83924900de8e
ItsFoxCrafter <163313829+ItsFoxCrafter@users.noreply.github.com>
2024-06-13 23:42:17 +0300

    Add files via upload

--- Home Page/main.js
+++ Home Page/main.js
@@ -7,3 +7,18 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+const handleOnMouseMove = e => {
+    const { currentTarget: target} = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clienty - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for (const card of document.querySelectorAll(".card")) {
+    card.onmousemove = e => handleOnMouseMove(e);
+}
