d977e1de4adff266a61a116a810b858596f24ecb
ItsFoxCrafter <163313829+ItsFoxCrafter@users.noreply.github.com>
2024-06-20 20:44:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);        
+    }
+}
